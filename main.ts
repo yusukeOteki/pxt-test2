c9fa4ccb7755f134290d2dbdf72a9983a3824213
ohteki <ohteki@syun.jp>
2018-11-19 06:36:47 +0900

    init

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 //% weight=70 icon="\u30A2" color=#EC7505 block="Test"
 namespace test {
 
-    /**
-    *  Writes to the Bluetooth UART service buffer.
-    */
     //% blockId=test block="test uart write %str"
     export function dammy(str: number): void {
-        console.log("test")
+        led.unplot(0, 0)
+        //console.log("test")
     }
 }
